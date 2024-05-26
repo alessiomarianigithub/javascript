37a82c412b6e3b19acd3c352d137a3e6e9220fb8
alessiomarianigithub alessiomarianigitpod <alessiogithub@gmail.com>
2024-05-26 14:52:40 +0000

    a

---
+++ Pila/pila.cpp
@@ -0,0 +1,88 @@
+ #ifndef PILA_H
+ #define PILA_H
+
+ class Pila 
+{
+    private:
+        int *vet;
+        int size;
+        int fine;
+    public#include <iostream>
+#include "Pila.h"
+
+using namespace std;
+
+Pila::Pila()
+{
+    vet=new int[10];
+    size=10;
+    fine=0;
+}
+
+Pila::Pila(int size)
+{
+    vet=new int[10];
+    this->size=size;
+    fine=0;
+}
+
+bool Pila::isEmpty()
+{
+    if(fine==0)return true;
+    return false;
+}
+
+bool Pila::isFull()
+{
+    if(fine==size)return true;
+    return false;
+}
+
+bool Pila::push(int x)
+{
+    if(!isFull())
+    {
+        vet[fine]=x;
+        fine++;
+        return true;
+    }
+    return false;
+}
+
+bool Pila::pop(void)
+{
+    if(!isEmpty())
+    {
+        fine--;
+        return true;
+    }
+    return false;
+}
+
+int Pila::top(void)
+{
+    if(!isEmpty())
+    {
+        return vet[fine-1];
+    }
+    return 0;
+}
+
+void Pila::stampa(void)
+{
+    if(!isEmpty())
+    {
+       for(int i=0;i<fine;i++) cout<<vet[i]<<endl;
+    }
+    
+}:
+        Pila();
+        Pila(int);
+        bool isEmpty();
+        bool isFull();
+        bool push(int);
+        bool pop(void);
+        int top(void);
+        void stampa(void);
+};
+#endif
